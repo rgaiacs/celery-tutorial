07ccc2178bf96ed2b69b86c2100607ce4ba69a11
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2024-07-15 16:27:21 +0200

    Add random sleep

--- src/tasks.py
+++ src/tasks.py
@@ -1,8 +1,15 @@
+import random
 import time
 
 from celery import shared_task
+from celery.signals import task_postrun
 
 
 @shared_task(ignore_result=False)
 def add(a, b):
+    time.sleep(random.randint(5, 30))
     return a + b
+
+@task_postrun.connect(sender=add)
+def task_postrun_notifier(sender=None, **kwargs):
+    print("From task_postrun_notifier ==> Ok, done!")
